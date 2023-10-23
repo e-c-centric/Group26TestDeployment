103e69ee12511d14214e2df2f13cbb8cbe621c10
MathiasPascal <elikem.gale-zoyiku@ashesi.edu.gh>
2023-10-23 18:00:52 +0000

    test

--- handlers/routes.py
+++ handlers/routes.py
@@ -1,9 +1,27 @@
-from flask import render_template
-from modules import modules
+import joblib
+import zipfile
+import os
+from flask import Flask, render_template, request
+import numpy as np
 
-def configure_routes(app):
-    @app.route("/")
-    def index():
-        hello = modules.hello()
-        content = modules.content()
-        return render_template("index.html", hello=hello, content=content)
+
+def index():
+    if request.method == "POST":
+        attributes = [
+            'movement_reactions', 'mentality_composure', 'passing',
+            'release_clause_eur', 'dribbling', 'wage_eur', 'power_shot_power',
+            'value_eur', 'mentality_vision', 'attacking_short_passing',
+            'physic', 'skill_long_passing', 'age'
+        ]
+
+        input_data = {}
+        for attr in attributes:
+            value = float(request.form.get(attr, 0))
+            input_data[attr] = value
+
+        prediction = ensemble.predict([list(input_data.values())])
+        prediction = round(prediction[0])
+
+        return render_template("result.html", prediction=prediction)
+
+    return render_template("form.html")
